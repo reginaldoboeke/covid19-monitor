3fa3c6383518ee925c3792119169d2d72cad1c36
Reginaldo Boeke <reginaldoboeke@gmail.com>
2020-07-02 00:07:41 -0300

    Desenvolvida tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Country {
+  Country: string;
+  TotalConfirmed: number | string;
+  TotalRecovered: number | string;
+  TotalDeaths: number | string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'freterapido-challenge';
+
+  countries: Country[];
+
+  constructor() {
+    this.countries = [
+      {
+        Country: 'Brasil',
+        TotalConfirmed: 111453,
+        TotalRecovered: 34562,
+        TotalDeaths: 43532,
+      },
+      {
+        Country: 'Brasil',
+        TotalConfirmed: 111453,
+        TotalRecovered: 34562,
+        TotalDeaths: 43532,
+      },
+      {
+        Country: 'Brasil',
+        TotalConfirmed: 111453,
+        TotalRecovered: 34562,
+        TotalDeaths: 43532,
+      },
+      {
+        Country: 'Brasil',
+        TotalConfirmed: 111453,
+        TotalRecovered: 34562,
+        TotalDeaths: 43532,
+      },
+      {
+        Country: 'Brasil',
+        TotalConfirmed: 111453,
+        TotalRecovered: 34562,
+        TotalDeaths: 43532,
+      },
+    ];
+
+    this.countries = this.countries.map(country => ({
+      ...country,
+      TotalConfirmed: country.TotalConfirmed.toLocaleString(),
+      TotalRecovered: country.TotalRecovered.toLocaleString(),
+      TotalDeaths: country.TotalDeaths.toLocaleString(),
+    }))
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AppSearchbarComponent } from './components/app-searchbar/app-searchbar.component';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppSearchbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
