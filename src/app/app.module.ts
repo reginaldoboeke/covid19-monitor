e525c8c7a3fad47839616f7f6e5d32af7094db63
Reginaldo Boeke <reginaldoboeke@gmail.com>
2020-07-05 20:42:45 -0300

    Alterado locale_id, adicionado httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe, registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -10,9 +11,13 @@ import { ChartsModule } from 'ng2-charts';
 import { HomePage } from './pages/home/home.page';
 import { DetailsPage } from './pages/details/details.page';
 
+import ptBR from '@angular/common/locales/pt';
+
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppSearchbarComponent } from './components/app-searchbar/app-searchbar.component';
+
+registerLocaleData(ptBR, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -26,11 +31,15 @@ import { AppSearchbarComponent } from './components/app-searchbar/app-searchbar.
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ReactiveFormsModule,
     FontAwesomeModule,
     ChartsModule,
   ],
-  providers: [DatePipe],
+  providers: [
+    DatePipe,
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
