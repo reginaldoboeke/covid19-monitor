d5badb2cdc48710258e193f6b00ec0b2409cdde3
Reginaldo Boeke <reginaldoboeke@gmail.com>
2020-07-06 22:46:52 -0300

    Finalizadas as funcionalidades da tela Details

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 import { ChartType, ChartOptions } from 'chart.js';
 import { Label } from 'ng2-charts';
+
+import { CountryStatistics } from 'src/app/models/country.model';
+import { DataService } from 'src/app/services/data.service';
+import { ToastrService } from 'ngx-toastr';
+
+interface UpdateChartData {
+  confirmed: number;
+  recovered: number;
+  deaths: number;
+}
 
 @Component({
   selector: 'app-details-page',
@@ -12,17 +24,13 @@ export class DetailsPage implements OnInit {
 
   pieChartOptions: ChartOptions = {
     responsive: true,
-    legend: {
-      position: 'top',
-    },
+    legend: { position: 'top' },
     layout: {
-      padding: {
-        top: 50,
-      },
+      padding: { top: 50 },
     },
     plugins: {
       datalabels: {
-        formatter: (value, ctx) => {
+        formatter: (_, ctx) => {
           const label = ctx.chart.data.labels[ctx.dataIndex];
           return label;
         },
@@ -32,27 +40,78 @@ export class DetailsPage implements OnInit {
   };
 
   pieChartLabels: Label[] = ['Infectados', 'Curados', 'Mortes'];
-  pieChartData: number[] = [300, 500, 100];
+  pieChartData: number[] = [0, 0, 0];
   pieChartType: ChartType = 'pie';
   pieChartLegend = true;
-  pieChartColors = [{
-    backgroundColor: ['#F54E70', '#6FD408', '#9476FF'],
-  }];
+  pieChartColors = [{ backgroundColor: ['#F54E70', '#6FD408', '#9476FF'] }];
 
-  constructor() { }
+  country: CountryStatistics = {} as CountryStatistics;
 
-  ngOnInit(): void { }
+  constructor(
+    private route: ActivatedRoute,
+    private datePipe: DatePipe,
+    private toastr: ToastrService,
+    private dataService: DataService,
+  ) { }
 
-  handleChangeData() {
-    this.pieChartData = [500, 100, 300];
+  ngOnInit(): void {
+    const {
+      Country,
+      Confirmed,
+      Deaths,
+      Recovered,
+      Date,
+      Slug,
+    } = this.route.snapshot.queryParams;
+
+    this.country.Country = Country;
+    this.country.Confirmed = Confirmed;
+    this.country.Deaths = Deaths;
+    this.country.Recovered = Recovered;
+    this.country.Date = Date;
+    this.country.Slug = Slug;
+
+    this.updateChartData({
+      confirmed: Number(Confirmed),
+      recovered: Number(Recovered),
+      deaths: Number(Deaths),
+    })
   }
 
-  handleGetData(date?: Date) {
-    console.log('handleGetData', date);
+  async getCountryStatisticsByDate(date: string) {
+    const [year, month, day] = date.split('-').map(Number);
+
+    const fromDate = new Date(year, month - 1, day);
+    const toDate = new Date(year, month - 1, day + 1)
+
+    fromDate.setUTCHours(0);
+    toDate.setUTCHours(0);
+
+    const currentDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+
+    if (date === currentDate) {
+      fromDate.setDate(fromDate.getDate() - 1);
+      toDate.setDate(toDate.getDate() - 1);
+    }
+
+    const [responseCountry] = await this.dataService.getCountryStatisticsByDate({
+      countrySlug: this.country.Slug,
+      fromDate,
+      toDate,
+    });
+
+    this.country = Object.assign(this.country, responseCountry);
+
+    this.updateChartData({
+      confirmed: this.country.Confirmed,
+      recovered: this.country.Recovered,
+      deaths: this.country.Deaths,
+    });
+
+    this.toastr.success(null, 'Dados atualizados!');
   }
 
-  handleRefresh() {
-    console.log('handleRefresh');
+  updateChartData({ confirmed, recovered, deaths }: UpdateChartData) {
+    this.pieChartData = [confirmed, recovered, deaths];
   }
-
 }
