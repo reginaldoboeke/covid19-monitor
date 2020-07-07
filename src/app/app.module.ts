71609b77fd45ace5cd680f0d4cdb798991d24c46
Reginaldo Boeke <reginaldoboeke@gmail.com>
2020-07-06 22:45:10 -0300

    Adicionar toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,17 +8,23 @@ import { DatePipe, registerLocaleData } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ChartsModule } from 'ng2-charts';
+import { ToastrModule, GlobalConfig as ToastrConfig } from 'ngx-toastr';
 
 import { HomePage } from './pages/home/home.page';
 import { DetailsPage } from './pages/details/details.page';
 
-import ptBR from '@angular/common/locales/pt';
+import localeBr from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppSearchbarComponent } from './components/app-searchbar/app-searchbar.component';
 
-registerLocaleData(ptBR, 'pt-BR');
+registerLocaleData(localeBr, 'pt');
+
+const toastrConfig: Partial<ToastrConfig> = {
+  progressBar: true,
+  timeOut: 2500,
+};
 
 @NgModule({
   declarations: [
@@ -35,10 +42,12 @@ registerLocaleData(ptBR, 'pt-BR');
     ReactiveFormsModule,
     FontAwesomeModule,
     ChartsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(toastrConfig),
   ],
   providers: [
     DatePipe,
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: LOCALE_ID, useValue: 'pt' },
   ],
   bootstrap: [AppComponent]
 })
