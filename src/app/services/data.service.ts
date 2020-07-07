afcd00c91a2be1f9edf17710fe67c6e8f447f573
Reginaldo Boeke <reginaldoboeke@gmail.com>
2020-07-06 22:47:38 -0300

    Adicionados novos métodos

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Summary } from '../models/summary.model';
+import { CountryStatistics } from '../models/country.model';
+import { GlobalStatistics } from '../models/global.model';
 
+interface GetCountryStatisticsByDateDTO {
+  countrySlug: string;
+  fromDate: Date;
+  toDate: Date;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -13,9 +20,29 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getSummary() {
+  getSummary(): Promise<Summary> {
     return this.http
       .get<Summary>(`${this.baseURL}/summary`)
+      .toPromise();
+  }
+
+  getCurrentWorldStatistic(): Promise<GlobalStatistics> {
+    return this.http
+      .get<GlobalStatistics>(`${this.baseURL}/world/total`)
+      .toPromise();
+  }
+
+  getCountryStatisticsByDate({
+    countrySlug,
+    fromDate,
+    toDate,
+  }: GetCountryStatisticsByDateDTO): Promise<CountryStatistics[]> {
+    const fromDateString = fromDate.toISOString();
+    const toDateString = toDate.toISOString();
+    const endpoint = `country/${countrySlug}?from=${fromDateString}&to=${toDateString}`;
+
+    return this.http
+      .get<CountryStatistics[]>(`${this.baseURL}/${endpoint}`)
       .toPromise();
   }
 }
